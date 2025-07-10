9e8c8fb747eaabd2d1a6ad6adb55b8eb58fcde85
Jackihyun <pkhjack2325@gmail.com>
2025-07-10 17:31:12 +0900

    feat: 초기 세팅완료

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white mx-auto w-1/2 text-3xl">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
