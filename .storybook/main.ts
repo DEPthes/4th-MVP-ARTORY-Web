96fe1286edc2534a1403cbe7c5fa804e8eec6424
Jackihyun <pkhjack2325@gmail.com>
2025-07-16 23:54:27 +0900

    chore: package-lock.json 및 스토리북 설정 파일 업데이트
    - @eslint/core 및 @eslint/js의 버전 업데이트
    - @rollup 관련 패키지 버전 업데이트
    - tailwindcss 관련 설정 제거 및 스토리북 설정 간소화
    - 새로운 의존성 추가 및 불필요한 의존성 제거
    - package-lock.json의 의존성 정리 및 업데이트

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,4 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import tailwindcss from "tailwindcss";
-import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -15,14 +13,6 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      css: {
-        postcss: {
-          plugins: [tailwindcss()],
-        },
-      },
-    });
-  },
 };
+
 export default config;
