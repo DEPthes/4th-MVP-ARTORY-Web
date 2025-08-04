dc678d66a6b0349e2e47845dbf9d24131cea1975
rhdmschdl <lovelyskygir@naver.com>
2025-08-05 03:00:48 +0900

    feat: 스토리북 파일 경로 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./.storybook/**/*.{js,ts}",
+  ],
   theme: {
     extend: {},
     fontFamily: {
