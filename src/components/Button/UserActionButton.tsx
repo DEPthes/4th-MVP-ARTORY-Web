3d678e9384f7272254e6cd646c530c3e2d4696cb
최고은 <cho2goeun@naver.com>
2025-08-04 17:38:58 +0900

    [Style/#4] 팔로잉 버튼 구현 (#5)
    * feat: 폰트 설정
    
    * chore: svgr 추가
    
    * feat: svgr 설정
    
    * feat: 폰트 설정
    
    * feat: 이미지 추가
    
    * feat: 팔로잉버튼 스토리북
    
    * feat: 팔로잉버튼 컴포넌트 구현

---
+++ src/components/Button/UserActionButton.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { cn } from "../../utils/classname";
+import FollowingIcon from "../../assets/following.svg?react";
+import FollowIcon from "../../assets/follow.svg?react";
+import ProfileEditIcon from "../../assets/profileEdit.svg?react";
+
+interface UserActionButtonProps {
+  initialType?: "following" | "follow" | "edit";
+  onClick?: () => void;
+  className?: string;
+}
+
+const UserActionButton: React.FC<UserActionButtonProps> = ({
+  initialType = "follow",
+  onClick,
+  className = "",
+}) => {
+  const [type, setType] = useState<"following" | "follow" | "edit">(
+    initialType
+  );
+
+  const handleClick = () => {
+    if (type === "following") setType("follow");
+    else if (type === "follow") setType("following");
+    if (onClick) onClick();
+  };
+
+  const renderContent = () => {
+    const iconClass = "mr-[6px] text-current";
+    switch (type) {
+      case "follow":
+        return (
+          <>
+            <FollowIcon className={iconClass} />
+            팔로우
+          </>
+        );
+      case "following":
+        return (
+          <>
+            <FollowingIcon className={iconClass} />
+            팔로잉
+          </>
+        );
+      case "edit":
+        return (
+          <>
+            <ProfileEditIcon className={iconClass} />
+            프로필 편집
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+  // 버튼 공통 스타일
+  const baseClasses =
+    "flex font-asta items-center justify-center w-[223px] h-[50px] border-2 border-[#D32F2F] rounded-full";
+
+  const variantClasses = {
+    following: "text-[#1D1E20] bg-[#D32F2F]/10",
+    follow: "bg-[#D32F2F] text-white",
+    edit: "text-[#1D1E20] bg-[#D32F2F]/10",
+  };
+
+  return (
+    <button
+      type="button"
+      className={cn(baseClasses, variantClasses[type], className)}
+      onClick={handleClick}
+    >
+      {renderContent()}
+    </button>
+  );
+};
+
+export default UserActionButton;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      asta: ["Asta Sans", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
