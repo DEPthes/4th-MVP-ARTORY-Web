54e9c8b81e2bb64b90e687e8fe91fc0f3ed7dc66
jj <kevin2483@naver.com>
2025-08-07 00:32:44 +0900

    fix:css 수정

--- src/components/ArtworkCard.tsx
+++ src/components/ArtworkCard.tsx
@@ -28,7 +28,7 @@ const ArtworkCard: React.FC<ArtworkCardProps> = ({
       )}
     >
       {/* 상단 이미지 */}
-      <div className="flex justify-center items-center bg-white pt-5 pr-5 pb-2 pl-5">
+      <div className="flex justify-center items-center bg-white pt-5 px-5 pb-2 ">
         <img
           src={imageUrl}
           alt={title}
@@ -37,19 +37,19 @@ const ArtworkCard: React.FC<ArtworkCardProps> = ({
       </div>
 
       {/* 작품명 */}
-      <div className="flex items-center pt-2 pb-2 pl-6 pr-6">
+      <div className="flex items-center py-2 px-6">
         <p className="font-semibold text-2xl text-zinc-900">{title}</p>
       </div>
 
       {/* 작가명 */}
       {author && (
-        <div className="flex items-center pt-2 pb-2 pl-6 pr-6">
+        <div className="flex items-center py-2 px-6">
           <p className="text-lg text-zinc-900">{author}</p>
         </div>
       )}
 
       {/* 좋아요 */}
-      <div className="flex justify-end items-center w-full pt-4 pb-4 pl-6 pr-6">
+      <div className="flex justify-end items-center w-full py-4 px-6">
         <LikeIcon className="size-4 text-red-500" />
         <span className=" text-zinc-900">{likes}</span>
       </div>
