584e2916a3c81d0826fd32914f6272e6bd3875af
rhdmschdl <lovelyskygir@naver.com>
2025-08-15 03:13:57 +0900

    Design: 작가노트 작성 후 UI 수정

--- src/components/Profile/DisplayEntry.tsx
+++ src/components/Profile/DisplayEntry.tsx
@@ -5,13 +5,9 @@ interface DisplayEntryProps {
 
 const DisplayEntry: React.FC<DisplayEntryProps> = ({ year, text }) => {
   return (
-    <div className="flex items-center space-x-3">
-      <div className="w-23.5 h-13.5 font-asta font-medium rounded-md px-7 py-4 bg-[#EAEBED]">
-        {year || "yyyy"}
-      </div>
-      <div className="font-asta font-thin w-full h-13.5 bg-[#F4F5F6] rounded-md px-5.5 py-4">
-        {text || "내용 없음"}
-      </div>
+    <div className="flex items-center space-x-6 bg-white">
+      <div className="w-10 font-asta font-medium">{year || "yyyy"}</div>
+      <div className="font-asta font-thin w-full">{text || "내용 없음"}</div>
     </div>
   );
 };
